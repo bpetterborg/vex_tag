b954ca586b0d0e1921dbb5ea6432e493323fa01d
bpetterborg <ben.petterborg@gmail.com>
2022-03-08 10:17:43 -0500

    Cleaned stuff up, being work on tag and exponential speed funcs

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,9 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       VEX                                                       */
-/*    Created:      Wed Sep 25 2019                                           */
-/*    Description:  Speedbot (4-motor Drivetrain, No Gyro)                    */
-/*                                                                            */
-/*    Name:                                                                   */
-/*    Date:                                                                   */
-/*    Class:                                                                  */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    1, 10, 11, 20   
-// Controller1          controller                    
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
 #include "vex.h"
-
 using namespace vex;
 
-int main() {
+int main() 
+{
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 }
+

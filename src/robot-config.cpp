42b7685bb210dc2f58734c583aa59d118ec80c11
bpetterborg <ben.petterborg@gmail.com>
2022-03-09 09:54:26 -0500

    Complete main motor control code. Need work on tagging

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -1,4 +1,5 @@
 #include "vex.h"
+#include "math.h"
 
 using namespace vex;
 using signature = vision::signature;
@@ -6,10 +7,12 @@ using code = vision::code;
 brain Brain;
 
 // VEXcode device constructors
+//bumper frontBumper = bumper(triport::port &port)
+
 motor leftMotorA = motor(PORT1, ratio18_1, false);
 motor leftMotorB = motor(PORT10, ratio18_1, false);
-
 motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB);
+
 motor rightMotorA = motor(PORT11, ratio18_1, true);
 motor rightMotorB = motor(PORT20, ratio18_1, true);
 motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB);
@@ -20,85 +23,101 @@ controller Controller1 = controller(primary);
 // some vars
 bool DrivetrainLNeedsToBeStopped_Controller1 = true;
 bool DrivetrainRNeedsToBeStopped_Controller1 = true;
-double finalSpeed;
 
 
+// exponential speed control left side
+int getLeftDriveSpeed (vex::directionType type, int percentage)
+{
+  if (percentage >= 0)
+  {
+    // when stationary, or going forwards, put the percentage in the exponential func
+    percentage = 1.2 * pow(1.043, percentage) - 1.2 + 0.2 * percentage;
+    return percentage; 
+  }
+  else
+  {
+    // when reversing, take the positive percentage, and put it in exponential func
+    percentage = -percentage;
+    percentage = 1.2 * pow(1.043, percentage) - 1.2 + 0.2 * percentage; 
+    
+    // when the new percentage val is calculated, make negative for backwards movement
+    percentage = -percentage;
+    return percentage;
+  }
+}
 
+// exponential speed control right side
+int getRightDriveSpeed (vex::directionType type, int percentage)
+{
+  if (percentage >= 0)
+  {
+    // when stationary, or going forwards, put the percentage in the exponential func
+    percentage = 1.2 * pow(1.043, percentage) - 1.2 + 0.2 * percentage;
+    return percentage; 
+  }
+  else
+  {
+    // when reversing, take the positive percentage, and put it in exponential func
+    percentage = -percentage;
+    percentage = 1.2 * pow(1.043, percentage) - 1.2 + 0.2 * percentage; 
+    
+    // when the new percentage val is calculated, make negative for backwards movement
+    percentage = -percentage;
+    return percentage;
+  }
+}
 
 
 // function that adjusts speed based on what the buttons do
-double getTagSpeed()
+int getTagMultiplier (int tagMultiplier)
 {
   
-
-  return finalSpeed;
+  return tagMultiplier;
 }
 
 
 
-
-// define a task that will handle monitoring inputs from Controller1
 int rc_auto_loop_function_Controller1() 
 {
   while(true) 
   {
-    int drivetrainLeftSideSpeed = Controller1.Axis3.position();
-    int drivetrainRightSideSpeed = Controller1.Axis2.position();
-    
-    // deadband
-    if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) 
-    {
-      if (DrivetrainLNeedsToBeStopped_Controller1) 
-      {
-        LeftDriveSmart.stop();
-        DrivetrainLNeedsToBeStopped_Controller1 = false;
-      }
-    } 
-    else 
-    {
-      // reset the toggle so that the deadband code knows to stop the left motor nexttime the input is in the deadband range
-      DrivetrainLNeedsToBeStopped_Controller1 = true;
-    }
-    
-    // check if the value is inside of the deadband range
-    if (drivetrainRightSideSpeed < 5 && drivetrainRightSideSpeed > -5) 
-    {
-      // check if the right motor has already been stopped
-      if (DrivetrainRNeedsToBeStopped_Controller1) 
-      {
-        // stop the right drive motor
-        RightDriveSmart.stop();
-        // tell the code that the right motor has been stopped
-        DrivetrainRNeedsToBeStopped_Controller1 = false;
+    int deadband = 2;
 
-      }
-    } 
-    else 
-    {
-      // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
-      DrivetrainRNeedsToBeStopped_Controller1 = true;
-    }
+    // still need to add speed multiplier
+    int leftMotorSpeed = getLeftDriveSpeed (vex::directionType::fwd, Controller1.Axis3.value());
+    int rightMotorSpeed = getRightDriveSpeed (vex::directionType::fwd, Controller1.Axis2.value());
     
-
-    // only tell the left drive motor to spin if the values are not in the deadband range
-    if (DrivetrainLNeedsToBeStopped_Controller1) 
+    // left side deadband checking
+    if (abs(leftMotorSpeed) < deadband)
     {
-      LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
-      LeftDriveSmart.spin(forward);
+      LeftDriveSmart.setVelocity(0, percent);
+    }
+    else
+    {
+      LeftDriveSmart.setVelocity(leftMotorSpeed, percent);
     }
 
-    // only tell the right drive motor to spin if the values are not in the deadband range
-    if (DrivetrainRNeedsToBeStopped_Controller1) 
+    // right side deadband checking
+    if (abs(rightMotorSpeed) < deadband)
     {
-      RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
-      RightDriveSmart.spin(forward);
+      RightDriveSmart.setVelocity(0, percent);
     }
-  
+    else
+    {
+      RightDriveSmart.setVelocity(rightMotorSpeed, percent);
+    }
+    
+    // spin both motors
+    RightDriveSmart.spin(forward);
+    LeftDriveSmart.spin(forward);
+
     wait(20, msec);
   }
 
   return 0;
 }
+
+
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
